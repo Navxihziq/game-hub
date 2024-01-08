74ea5da9e2a92324a1ab5ea9c1ca0d9dc0954e33
齐智轩 <zhixuanqi@outlook.com>
2024-01-07 20:19:59 -0500

    added navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,14 +4,13 @@ import { ThemeToggle } from "./ThemeToggle";
 export const NavBar = () => {
   return (
     <>
-      <HStack>
+      <HStack justifyContent="space-between" padding="10px">
         <Image
           w="10vw"
           maxW="60px"
           src="./src/assets/logo.webp"
           alt="logo of GameHub"
         />
-        <Text>Nav Bar</Text>
         <ThemeToggle />
       </HStack>
     </>
--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,16 +1,16 @@
-import { useColorMode, Switch, Text } from "@chakra-ui/react";
+import { useColorMode, Switch, Text, HStack } from "@chakra-ui/react";
 
 export const ThemeToggle = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <>
+    <HStack>
       <Switch
         colorScheme="blue"
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
       />
       <Text>Dark Mode</Text>
-    </>
+    </HStack>
   );
 };
