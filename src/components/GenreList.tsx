204eb93c356030812f9c87f91d97ad4b0db0d7ec
齐智轩 <zhixuanqi@outlook.com>
2024-01-12 00:22:40 -0500

    added genre list

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,23 @@
+import { List, ListIcon, ListItem } from "@chakra-ui/react";
+
 import useGenres from "../hooks/useGenres";
 
 function GenreList() {
   const { data } = useGenres();
   console.log(data);
-  return <>{data && data.map((genre) => <p>{genre.name}</p>)}</>;
+  return (
+    <>
+      <List>
+        {data &&
+          data.map((genre) => (
+            <ListItem>
+              <ListIcon />
+              {genre.name}
+            </ListItem>
+          ))}
+      </List>
+    </>
+  );
 }
 
 export default GenreList;
